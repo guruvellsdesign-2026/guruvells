a3df3aeb5774e6fe04baf23eadbf7a8038925d1e
guruvellsdesign-2026 <guruvellsdesign-2026@users.noreply.github.com>
2026-03-09 07:58:15 +0530

    chore: updated sanity cli config and dependencies

--- sanity.cli.ts
+++ sanity.cli.ts
@@ -4,5 +4,9 @@ export default defineCliConfig({
   api: {
     projectId: 'do4k1nvo',
     dataset: 'production'
-  }
+  },
+  autoUpdates: true,
+  deployment: {
+    appId: 'lmrkpno2os2ktlli1xj0yi6t',
+  },
 });
